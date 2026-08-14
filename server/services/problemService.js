6bcf358facfc0647d6e165954b7210b80775161a
mayankyadav19 <hatmario06@gmail.com>
2026-08-14 19:52:14 +0530

    feat: implement save and get problem methods

--- server/services/problemService.js
+++ server/services/problemService.js
@@ -1,7 +1,24 @@
 const ProblemModel = require("../models/problemSchema");
 
 class ProblemService {
-    saveProblem (problemObject) {
-        
+    async saveProblem (problemObject) {
+        try {
+            const newProblem = new ProblemModel(problemObject);
+            return await newProblem.save();
+        } catch (error) {
+            throw new Error (`Failed to save problem :${error.message}`);
+        }
+    }
+
+    async getProblem (problemId) {
+        try {
+            const problem = await ProblemModel.findById(problemId);
+            if(!problem) throw new Error ('Problem not found');
+            return problem;
+        } catch (error) {
+            throw new Error (`Error fetching problem: ${error.message}`);
+        }
     }
 }
+
+module.exports = ProblemService;
