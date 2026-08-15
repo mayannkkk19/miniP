a074710e0c426406ab2e15c5ecb85178f967d3ed
mayankyadav19 <hatmario06@gmail.com>
2026-08-16 00:14:42 +0530

    refactor: move submit problem controller into separate file

---
+++ server/controller/problemController.js
@@ -0,0 +1,32 @@
+const Problem = require("../models/problem");
+const ProblemService = require('../services/problemService');
+
+const problemService = new ProblemService();
+
+exports.saveProblem = async (req, res, next) => {
+    console.log("Came to post a problem");
+
+    const validation = problemService.validateProblem(req.body);
+
+    if(!validation.success) {
+        console.log(validation.message);
+        res.status(400);
+        return res.send (`<h1>Problem upload failed, invalid data</h1>`);
+    }else{
+        console.log(validation.message);
+    }
+
+    const problem = new Problem(req.body);
+    //console.log(problem);
+
+    try{
+        const problemObject = await problemService.saveProblem(problem);
+        console.log(problemObject._id);
+    } catch (error) {
+        console.log(error.message);
+        res.status(400);
+        return res.send(`<h1>Problem upload failed, invalid data</h1>`)
+    }
+    res.status(201);
+    return res.send(`<h1>Problem uploaded</h1>`);
+}
